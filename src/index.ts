a9c66468a91cf601c9ddf06265f523bab40cd23d
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2018-11-28 22:30:45 +0800

    add ts-node, nodemon support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/*  tslint:disable:no-expression-statement no-console */
+
 import Add from './util/add.class';
 import Subtract from './util/subtract.class';
 
@@ -6,3 +8,4 @@ const subOp = new Subtract();
 
 console.log('3 + 5:', addOp.add(3,5));
 console.log('2 - 5:', subOp.subtract(2,5));
+console.log('what?', addOp.add(3, 9));
